a74fdc8b0e19a260284ecfb41acb0c6aca845230
Jordan Milne <jordan.milne@saynotolinux.com>
2014-12-03 04:13:22 -0400

    Add a convenience wrapper around `PullRequestEvent` payloads

--- stinkypy/ghtools.py
+++ stinkypy/ghtools.py
@@ -15,3 +15,15 @@ def get_gh_session(access_token):
     sess.auth = (access_token, 'x-oauth-basic')
     sess.headers['User-Agent'] = 'stinkypy by @JordanMilne/0.1'
     return sess
+
+
+class PullRequestEvent(dict):
+    """Simple wrapper around the JSON returned by the PR webhook"""
+
+    def getLineLink(self, file_path, line_range):
+        base_url = self["pull_request"]["url"]
+        return base_url + "#" + gen_diff_anchor(file_path, line_range)
+
+    def getDiff(self, session):
+        headers = {"Accept": "application/vnd.github.3.diff"}
+        return session.get(self["pull_request"]["url"], headers=headers).text
